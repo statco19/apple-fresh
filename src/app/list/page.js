238361c2d76569a86680fe01ba8d034f1ce728c2
andrew <statco19@gmail.com>
2024-04-28 13:12:38 +0900

    learned Array.map (lecture 4)

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,13 +1,15 @@
 export default function List() {
+  let products = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
       <h2 className="title">상품목록</h2>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {products.map((item, idx) => {
+        return (
+          <div className="food" key={idx}>
+            <h4>{item} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
